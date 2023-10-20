71475aca2f6dda637ff81bade16000f53d60784b
Oleksandr <oleksandrburtovyi@gmail.com>
2023-10-20 23:21:29 +0200

    FinishGallery#1

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const list = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem => `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+list.innerHTML = markup;
+
+const openImage = e => {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.preventDefault();
+
+  const srcFullImage = e.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${srcFullImage}">`);
+  instance.show();
+  const closeImage = ev => {
+    if (ev.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', closeImage);
+    }
+  };
+  window.addEventListener('keydown', closeImage);
+};
+
+list.addEventListener('click', openImage);
